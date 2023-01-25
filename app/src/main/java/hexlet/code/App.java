e55c3a22b85aae430fffbc56bd3418d8a96e011e
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2023-01-25 12:45:25 +0300

    Add changes on 5 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,32 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.Greetings();
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        int num = in.nextInt();
+        System.out.println("Your choice: " + num);
+        switch (num) {
+            case 0:
+                System.out.println("Bye!");
+                break;
+            case 1:
+                Scanner in_1 = new Scanner(System.in);
+                System.out.println("Welcome to the Brain Games!");
+                System.out.print("May I have your name? ");
+                String userName = in_1.next();
+                System.out.println("Hello, " + userName + "!");
+                in_1.close();
+                break;
+            case 2:
+                Even.ParityCheck();
+        }
+        in.close();
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,42 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void ParityCheck() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = in.next();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        int countOfVictories = 0;
+        for (int i = 0; i < 3; i++) {
+            Scanner in1 = new Scanner(System.in);
+            int number = (int) (Math.random() * 50);
+            System.out.println("Question: " + number);
+            String answer = in1.next();
+            System.out.println("Your answer: " + answer);
+
+            if (number % 2 == 0 && answer.equals("yes")) {
+                System.out.println("Correct!");
+                countOfVictories++;
+            }
+            else if (number % 2 != 0 && answer.equals("no")) {
+                System.out.println("Correct!");
+                countOfVictories++;
+            } else {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+                System.out.println("Let's try again, " + userName + "!");
+            }
+            if (i == 2) {
+                in1.close();
+            }
+        }
+        int CheckNumber = 3;
+        if (countOfVictories == CheckNumber) {
+            System.out.println("Congratulations, " + userName + "!");
+        }
+        in.close();
+    }
+}
