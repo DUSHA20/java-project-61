386afce2ef8359bb4d818436b76461dd9220ec3e
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2023-09-11 11:56:35 +0300

    Add chaches in Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,8 +7,7 @@ public class Cli {
     public static void greetings() {
 
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Hello, world!");
+        
         System.out.print("What is your name? ");
         String userName = scanner.next();
         System.out.println();
