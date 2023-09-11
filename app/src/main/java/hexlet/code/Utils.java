6279c057a3c6f707093ba4d5d4622bf11927965c
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2023-09-11 17:47:06 +0300

    Code style corrections have been made 2

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Utils {
 
-    private static final Random randomNumber = new Random();
+    private static final Random RANDOM = new Random();
 
     public static int getRandomNumberInRange(int min, int max) {
 
-        return randomNumber.nextInt((max - min) + 1) + min;
+        return RANDOM.nextInt((max - min) + 1) + min;
     }
 
 }
